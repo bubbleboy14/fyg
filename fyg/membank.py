aaf76b0ce6448200f20b750930a73e17bda3f5ef
bubbleboy14 <mario.balibrera@gmail.com>
2024-01-17 16:59:20 -0800

    membank: MemBank class; getbank() and membanks{}; remember()/recall()/memget() take bank (default "default") kwarg and utilize getbank()-acquired MemBank instance

--- fyg/membank.py
+++ fyg/membank.py
@@ -1,25 +1,59 @@
+import os
 from .util import read, write
 
-membank = {}
-remembered = read(".membank")
-remembered and membank.update(remembered)
+class MemBank(object):
+	def __init__(self, bank="default"):
+		self.name = bank
+		self.path = os.path.join(".membank", bank)
+		self.bank = {}
+		self.load()
 
-def remember(key, data, ask=True):
-	if ask and input("remember %s for next time? [Y/n] "%(key,)).lower().startswith("n"):
-		return print("ok, not remembering", key)
-	membank[key] = data
-	write(".membank", membank)
+	def log(self, *msg):
+		print("MemBank(%s)"%(self.name,), *msg)
 
-def recall(key):
-	return membank.get(key, None)
+	def load(self):
+		if not os.path.isdir(".membank"):
+			self.log("load() creating .membank/")
+			movebank = os.path.isfile(".membank")
+			movebank and self.log("load() renaming default")
+			movebank and os.rename(".membank", ".membank-tmp")
+			os.mkdir(".membank")
+			movebank and os.rename(".membank-tmp", os.path.join(".membank", "default"))
+		remembered = read(self.path)
+		remembered and self.bank.update(remembered)
+		self.log("loaded", self.name, "bank")
 
-def memget(key, default=None):
-	val = recall(key)
-	if not val:
-		pstr = "%s? "%(key,)
-		if default:
-			pstr = "%s[default: %s] "%(pstr, default)
-		val = input(pstr) or default
-		remember(key, val)
-	return val
+	def remember(self, key, data, ask=True):
+		if ask and input("remember %s for next time? [Y/n] "%(key,)).lower().startswith("n"):
+			return self.log("ok, not remembering", key)
+		self.bank[key] = data
+		write(self.path, membank)
 
+	def recall(self, key):
+		return self.bank.get(key, None)
+
+	def get(self, key, default=None):
+		val = self.recall(key)
+		if not val:
+			pstr = "%s? "%(key,)
+			if default:
+				pstr = "%s[default: %s] "%(pstr, default)
+			val = input(pstr) or default
+			self.remember(key, val)
+		return val
+
+membanks = {}
+
+def getbank(name="default"):
+	if name not in membanks:
+		membanks[name] = MemBank(name)
+	return membanks[name]
+
+def remember(key, data, ask=True, bank="default"):
+	getbank(bank).remember(data, ask)
+
+def recall(key, bank="default"):
+	return getbank(bank).recall(key)
+
+def memget(key, default=None, bank="default"):
+	return getbank(bank).get(key, default)

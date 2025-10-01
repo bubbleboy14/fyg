e1147328d330020bf7cbc4e25ee4a24a9c204ad9
bubbleboy14 <mario.balibrera@gmail.com>
2025-10-01 13:40:40 -0700

    vpush 0.1.7.5

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 FancY confiG
 
-v0.1.7.4
+v0.1.7.5
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fyg',
-    version="0.1.7.4",
+    version="0.1.7.5",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',

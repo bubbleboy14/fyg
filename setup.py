d20f2e5e55e75439e6dd44b4c28a12527fe4f48c
bubbleboy14 <mario.balibrera@gmail.com>
2025-11-07 19:13:10 -0800

    vpush 0.1.7.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fyg',
-    version="0.1.7.7",
+    version="0.1.7.8",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
